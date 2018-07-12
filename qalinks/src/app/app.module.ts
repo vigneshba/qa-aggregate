fc069eea59372efbe1c95710b93347c53b7faee3
Aaqib <aaqib.hyd@gmail.com>
2018-07-11 21:00:50 -0300

    header - test

--- qalinks/src/app/app.module.ts
+++ qalinks/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
